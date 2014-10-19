6f630d4a7c4a65efb5bd6434fc523de8858a8ff6
Anja Berens <pharaun666@gmail.com>
2014-10-18 21:49:33 -0700

    this is probably terrible code

--- src/Example.hs
+++ src/Example.hs
@@ -3,6 +3,7 @@ import Network.NSQ.Types
 import Network.NSQ.Connection
 
 import Control.Applicative
+import Control.Concurrent
 import Control.Concurrent.Async
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TVar
@@ -18,6 +19,16 @@ import System.Log.Formatter
 -- Benchmark generation
 import Options.Applicative
 import Network
+import qualified Data.ByteString.Lazy.Builder as BL
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BL
+import qualified Pipes.Prelude as P
+import qualified Pipes as P
+import Pipes ((>~), (>->))
+import Data.Time.Clock
+import Data.Time.Format
+import System.Locale
+import System.IO
 
 
 main :: IO ()
@@ -62,7 +73,7 @@ prod s d = do
     -- Connect and start producing stuff
     race_
         (establish conf' topicQueue replyQueue)
-        (generateMessages s d topicQueue replyQueue)
+        (generateMessages s d replyQueue)
 
 cons :: Config -> TVar Bool -> IO ()
 cons s d = do
@@ -80,9 +91,11 @@ cons s d = do
         writeTQueue replyQueue $ Rdy (fromInteger $ sRdyNum s)
 
     -- Connect
-    race_
-        (establish conf' topicQueue replyQueue)
-        (consumeMessages s d topicQueue replyQueue)
+    withFile "benchmark.log" AppendMode (\h -> do
+        race_
+            (establish conf' topicQueue replyQueue)
+            (consumeMessages s h d topicQueue replyQueue)
+        )
 
 --data Config = Config
 --    { hostname :: String
@@ -92,32 +105,82 @@ cons s d = do
 --    , size :: Integer
 --    , rdyNum :: Integer
 --    }
-consumeMessages :: Config -> TVar Bool -> TQueue Message -> TQueue Command -> IO ()
-consumeMessages s d q r = forever $ do
-    msg <- atomically (do
+consumeMessages :: Config -> Handle -> TVar Bool -> TQueue Message -> TQueue Command -> IO ()
+consumeMessages s h d q r = do
+    (msg, death) <- atomically (do
+        die <- readTVar d
         m <- readTQueue q
-        -- Process data here
 
-        -- TODO: Unsafe, assumes it only get Messages (true as of current implementation, but still unsafe)
         writeTQueue r $ Fin $ mId m
         writeTQueue r $ Rdy 1
-        return m)
-    infoM "Client.Consume" (show msg)
+        return (m, die))
+
+    utc <- P.liftIO getCurrentTime
+
+    -- TODO: probably suboptimal and slow
+    BL.hPut h $ BL.toLazyByteString (
+            (BL.string8 $ formatTime defaultTimeLocale "%s%Q" utc)
+            <> BL.char8 ' '
+            -- TODO: truncate the remaining data
+            <> (BL.byteString $ mMsg msg)
+        )
+    BL.hPut h "\n"
+
+    -- Unless death reloop
+    unless death (consumeMessages s h d q r)
 
     where
         mId (Message _ _ mesgId _) = mesgId
+        mMsg (Message _ _ _ m) = m
 
---data Config = Config
---    { hostname :: String
---    , port :: PortNumber
---    , count :: Integer
---    , rate :: Integer
---    , size :: Integer
---    , rdyNum :: Integer
---    }
-generateMessages :: Config -> TVar Bool -> TQueue Message -> TQueue Command -> IO ()
-generateMessages s d q r = undefined
+generateMessages :: Config -> TVar Bool -> TQueue Command -> IO ()
+generateMessages s d r = do
+    inc <- newTVarIO 0
+    P.runEffect (
+        generateData (sSize s) >->
+        P.take (sCount s) >->
+        sleepMsg (sRate s) >->
+        timeStampMsg inc >->
+        dumpQueue r
+        )
 
+    -- Sleep for 1 minute then set TVar (d) to true
+    P.liftIO $ threadDelay (1000000*60)
+    P.liftIO $ atomically $ writeTVar d True
+    return ()
+
+generateData :: Monad m => Integer -> P.Producer BS.ByteString m r
+generateData s = return "dummy-data" >~ P.cat
+
+dumpQueue :: (Monad m, P.MonadIO m) => TQueue Command -> P.Consumer Command m ()
+dumpQueue q = forever $ do
+    msg <- P.await
+    P.liftIO $ atomically $ writeTQueue q msg
+
+sleepMsg :: (Monad m, P.MonadIO m) => Int -> P.Pipe BS.ByteString BS.ByteString m ()
+sleepMsg r = forever $ do
+    msg <- P.await
+    P.liftIO $ threadDelay (1000000 `div` r)
+    P.yield msg
+
+timeStampMsg :: (Monad m, P.MonadIO m) => TVar Int -> P.Pipe BS.ByteString Command m ()
+timeStampMsg inc = forever $ do
+    msg <- P.await
+    id <- P.liftIO $ atomically (do
+            id <- readTVar inc
+            writeTVar inc (id + 1)
+            return id
+        )
+    utc <- P.liftIO getCurrentTime
+
+    -- TODO: probably suboptimal and slow
+    P.yield $ Pub "benchmark" $ BL.toStrict $ BL.toLazyByteString (
+            (BL.string8 $ formatTime defaultTimeLocale "%s%Q" utc)
+            <> BL.char8 ' '
+            <> (BL.string8 $ show id)
+            <> BL.char8 ' '
+            <> BL.byteString msg
+        )
 
 
 withFormatter :: GenericHandler Handle -> GenericHandler Handle
@@ -135,8 +198,8 @@ withFormatter handler = setFormatter handler formatter
 data Config = Config
     { sHostname :: String
     , sPort :: PortNumber
-    , sCount :: Integer
-    , sRate :: Integer
+    , sCount :: Int
+    , sRate :: Int
     , sSize :: Integer
     , sRdyNum :: Integer
     }
